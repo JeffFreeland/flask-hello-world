f5d644813c6abb14c492033fdf5fd7655397778d
Jeffrey <test@example.com>
2021-08-05 16:57:16 -0500

    added a file to be read from by the service

--- flask-app/index.py
+++ flask-app/index.py
@@ -19,5 +19,16 @@ def env_var_2():
     DEFAULT_ENV_VAR = "ENV_VAR_2_NOT_SET"
     return os.getenv('ENV_VAR_2', default=DEFAULT_ENV_VAR)
 
+# This returns only the first line of the file
+@app.route("/from-file")
+def from_file():
+    DEFAULT_FILE_NAME = "config_file.txt"
+    FILE_NAME = os.getenv('FILE_NAME', default=DEFAULT_FILE_NAME)
+    with open(FILE_NAME, "r") as file:
+        line = file.readline()
+        return line
+    return "Something went wrong"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
